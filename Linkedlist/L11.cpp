9f3f266482c28e3bd663e2b7261a70f32202c1f6
harsh0620 <harsh0111chandravanshi@gmail.com>
2021-08-23 11:10:06 +0530

    L11

---
+++ Linkedlist/L11.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+Node *inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+
+    cin >> val;
+    head = tail = new Node(val);
+
+    while (--size)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+void printList(Node *n)
+{
+    while (n)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+Node *findIntersection(Node *head1, Node *head2);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        Node *head1 = inputList(n);
+        Node *head2 = inputList(m);
+
+        Node *result = findIntersection(head1, head2);
+
+        printList(result);
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+Node *insertAtEnd(Node *head, int x)
+{
+    Node *temp = new Node(x);
+    if (head == NULL)
+    {
+        return temp;
+    }
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    return head;
+}
+
+Node *findIntersection(Node *head1, Node *head2)
+{
+    Node *head = NULL;
+    while (head1 != NULL && head2 != NULL)
+    {
+        int x = head1->data;
+        int y = head2->data;
+        if (x == y)
+        {
+            head = insertAtEnd(head, head1->data);
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        else if (x < y)
+        {
+            head1 = head1->next;
+        }
+        else
+        {
+            head2 = head2->next;
+        }
+    }
+    return head;
+}
