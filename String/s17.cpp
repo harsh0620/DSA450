31fd4f9946d3bb7a83dd0dc0d6d0a4a8ca7637fd
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2022-01-24 17:04:50 +0530

    s17.cpp

---
+++ String/s17.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> search(string s, string t)
+    {
+        // code here.
+        const int p = 31;
+        const int m = 1e9 + 9;
+        int S = s.size(), T = t.size();
+
+        vector<long long> p_pow(max(S, T));
+        p_pow[0] = 1;
+        for (int i = 1; i < (int)p_pow.size(); i++)
+            p_pow[i] = (p_pow[i - 1] * p) % m;
+
+        vector<long long> h(T + 1, 0);
+        for (int i = 0; i < T; i++)
+            h[i + 1] = (h[i] + (t[i] - 'a' + 1) * p_pow[i]) % m;
+        long long h_s = 0;
+        for (int i = 0; i < S; i++)
+            h_s = (h_s + (s[i] - 'a' + 1) * p_pow[i]) % m;
+
+        vector<int> occurences;
+        for (int i = 0; i + S - 1 < T; i++)
+        {
+            long long cur_h = (h[i + S] + m - h[i]) % m;
+            if (cur_h == h_s * p_pow[i] % m)
+                occurences.push_back(i + 1);
+        }
+        if (occurences.size() > 0)
+        {
+            return occurences;
+        }
+        occurences.push_back(-1);
+        return occurences;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector<int> res = ob.search(pat, S);
+        for (int i : res)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
