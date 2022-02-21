f845921ceb20dd1b5c5f856e7a3d3b69261b70fb
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2022-02-21 10:36:27 +0530

    s42.cpp

---
+++ String/s42.cpp
@@ -0,0 +1,55 @@
+#include <string.h>
+
+using namespace std;
+#define MAX_CHARS 256
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to check if two strings are isomorphic.
+    bool areIsomorphic(string str1, string str2)
+    {
+        if (str1.size() != str2.size())
+        {
+            return false;
+        }
+        int fi[MAX_CHARS] = {0};
+        int se[MAX_CHARS] = {0};
+        int freq[str1.size()] = {0};
+        for (int i = 0; i < str1.size(); i++)
+        {
+            fi[str1[i] - 'a']++;
+            se[str2[i] - 'a']++;
+        }
+
+        for (int i = 0; i < str1.size(); i++)
+        {
+
+            if (fi[str1[i] - 'a'] != se[str2[i] - 'a'])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t;
+    cin >> t;
+    string s1, s2;
+    while (t--)
+    {
+        cin >> s1;
+        cin >> s2;
+        Solution obj;
+        cout << obj.areIsomorphic(s1, s2) << endl;
+    }
+
+    return 0;
+} // } Driver Code Ends
