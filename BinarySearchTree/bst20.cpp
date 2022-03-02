745c36a9db78c2e68fe50726479295d0ab1b55c6
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2022-03-02 14:02:30 +0530

    bst20.cpp

---
+++ BinarySearchTree/bst20.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void insert(Node **tree, int val)
+{
+    Node *temp = NULL;
+    if (!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if (val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if (val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+}
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root)
+        return 0;
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+        return 1 + getCountOfNode(root->left, l, h) +
+               getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+        return getCountOfNode(root->right, l, h);
+
+    else
+        return getCountOfNode(root->left, l, h);
+}
+
+bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        Node *root;
+        Node *tmp;
+        // int i;
+
+        root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++)
+        {
+            int k;
+            cin >> k;
+            insert(&root, k);
+        }
+
+        cout << isDeadEnd(root);
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
+
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+bool helper(Node *node, int min, int max)
+{
+    if (!node)
+        return (false);
+    if (min == max)
+        return (true);
+
+    bool a = helper(node->left, min, node->data - 1);
+    bool b = helper(node->right, node->data + 1, max);
+    return (a || b);
+}
+
+bool isDeadEnd(Node *root)
+{
+    return helper(root, 1, INT_MAX);
+}
